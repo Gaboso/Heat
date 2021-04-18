ab6981037436b2a28631203a10af3f38507d5b72
Gabriel de Carvalho <gabriel.carvalho@softplan.com.br>
2021-04-18 13:45:35 -0300

    Created spring base project

---
+++ service/src/main/java/com/github/gaboso/heat/HeatApplication.java
@@ -0,0 +1,14 @@
+package com.github.gaboso.heat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HeatApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeatApplication.class, args);
+    }
+
+}
